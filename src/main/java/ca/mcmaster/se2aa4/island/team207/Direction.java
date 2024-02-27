f7eb118d617186575b17dde36c337f8ce1677723
Ali Zain <ali.zain04@yahoo.com>
2024-02-26 23:52:24 -0500

    Testing Direction class, similar to how it was done in maze explorer

--- src/main/java/ca/mcmaster/se2aa4/island/team207/Direction.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team207/Direction.java
@@ -1,9 +1,43 @@
 package ca.mcmaster.se2aa4.island.team207;
 
-public class Direction {
-  
-  public void direction() {
-    //direction
+public enum Direction {
+  north, south, east, west;
+
+  public static Direction turnAround(Direction d) {
+      if (d == north) {
+          return south;
+      } else if (d == south) {
+          return north;
+      } else if (d == east) {
+          return west;
+      } else {
+          return east;
+      }
+  }
+
+  public static Direction turnLeft(Direction d) {
+      if (d == north) {
+          return west;
+      } else if (d == south) {
+          return east;
+      } else if (d == east) {
+          return north;
+      } else {
+          return south;
+      }
+  }
+
+  public static Direction turnRight(Direction d) {
+      if (d == north) {
+          return east;
+      } else if (d == south) {
+          return west;
+      } else if (d == east) {
+          return south;
+      } else {
+          return north;
+      }
   }
   
 }
+  
