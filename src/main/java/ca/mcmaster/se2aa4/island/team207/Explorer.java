a50296b7a79b0cd59495069cd2ae8146eeb09be9
coolguy-stack <nafieu.alif@gmail.com>
2024-03-03 17:31:18 -0500

    abstracted the decision making into Decision.java for encapsulation (closes #17, closes #21)

--- src/main/java/ca/mcmaster/se2aa4/island/team207/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team207/Explorer.java
@@ -13,13 +13,16 @@ public class Explorer implements IExplorerRaid {
 
     private final Logger logger = LogManager.getLogger();
     
-    private Integer counter = 0;
+    // private Integer counter = 0;
 
-    private String decisionMade = " "; 
+    // private String decisionMade = " "; 
 
-    private Integer newResult = 200;
+    // private Integer newResult = 200;
 
-    private boolean foundGround = false;
+    // private boolean foundGround = false;
+
+    private Decision decision = new Decision(); 
+
 
     @Override
     public void initialize(String s) {
@@ -34,109 +37,16 @@ public class Explorer implements IExplorerRaid {
 
     @Override
     public String takeDecision() {
-
-        // Step 1: fly, echo south in a loop until we return not "out of range"
-        // Step 2: when we echo land, head south and fly, echo in loop until on land
-        // Step 3: fly south, scan, echo in direction in a loop until we are out of land
-        // Step 4: head east, then head north and repeat process
-
-        JSONObject decision = new JSONObject();
-
-        // if (counter < 5) {
-        //     decision.put("action", "fly");
-        // }
-        // else {
-        //     decision.put("action", "stop");
-        // }
-        if (!foundGround){
-            if (newResult != 200){
-                foundGround = true;
-                decision.put("action", "heading");
-                decision.put("parameters", new JSONObject().put("direction", "S"));
-    
-            }
+        return decision.decisionControl();
         
-            else if (decisionMade.equals("fly")){
-                decision.put("action", "echo");
-                decision.put("parameters", new JSONObject().put("direction", "S"));
-            } 
-            else if (decisionMade.equals("echo")){
-                decision.put("action", "scan");
-            }
-            else{
-                decision.put("action", "fly");
-            }   
-        }
-        else {
-            if (newResult == 0){
-                decision.put("action", "stop");
-            }
-            else if (decisionMade.equals("fly")){
-                decision.put("action", "echo");
-                decision.put("parameters", new JSONObject().put("direction", "S"));
-            } 
-            else if (decisionMade.equals("echo")){
-                decision.put("action", "scan");
-            }
-            else {
-                decision.put("action", "fly");
-            } 
-        }
-        
-        // if (counter > 30) {
-        //     if (counter == 31) {
-        //         decision.put("action", "heading");
-        //         decision.put("parameters", new JSONObject().put("direction", "S"));                
-        //     }
-        //     else if (counter > 82) {
-        //         if (counter > 132) {
-        //             decision.put("action", "stop");
-        //         }
-        //         else if (counter % 3 == 0) {
-        //             decision.put("action", "fly");
-        //         }
-        //         else if (counter % 3 == 1) {
-        //             decision.put("action", "scan");
-        //         }
-        //         else {
-        //             decision.put("action", "echo");
-        //             decision.put("parameters", new JSONObject().put("direction", "S"));
-        //         }
-        //     }
-        //     else if (counter % 2 == 0) {
-        //         decision.put("action", "fly");
-        //     }
-        //     else {
-        //         decision.put("action", "echo");
-        //         decision.put("parameters", new JSONObject().put("direction", "S"));
-        //     }
-        // }
-        // else if (counter % 2 == 0) {
-        //     decision.put("action", "fly");
-        // }
-        // else {
-        //     decision.put("action", "echo");
-        //     decision.put("parameters", new JSONObject().put("direction", "S"));
-        // }        
-
-        logger.info("** Counter: " + counter);
-        counter += 1;
-        
-        String decisionString = decision.toString();
-        decisionMade = decision.getString("action");
-
-        logger.info("** Decision: {}",decisionString);
-        return decisionString;
     }
 
     @Override
     public void acknowledgeResults(String s) {
         Result result = new Result();
-        JSONObject init = result.printResult(s);
-        if (decisionMade == "echo"){
-            newResult = result.echo_result(init);
-            logger.info("Echo result: {}", newResult);
-        }
+        JSONObject resultData = result.printResult(s);
+        decision.useResults(resultData);
+        
         //int echo = result.echo_result(init);
         //logger.info("Echo result: {}", echo);
 
